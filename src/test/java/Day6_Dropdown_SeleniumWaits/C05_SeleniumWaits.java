f8e2620ee4d56f61c7695ae94d90535e67f1046c
mehmet20251990 <mehmet20251990@gmail.com>
2022-12-21 23:42:07 +0300

    21.12 Update-2

---
+++ src/test/java/Day6_Dropdown_SeleniumWaits/C05_SeleniumWaits.java
@@ -0,0 +1,29 @@
+package Day6_Dropdown_SeleniumWaits;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.BaseTest;
+
+import java.time.Duration;
+
+public class C05_SeleniumWaits extends BaseTest {
+
+    // Explicit wait
+
+    @Test
+    public void webDriverWait(){
+
+        driver.get("https://demoqa.com/elements");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        WebElement category = driver.findElement(By.xpath("(//div[@class='icon'])[2]"));
+
+        wait.until(ExpectedConditions.elementToBeClickable(category)).click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(.,'Practice Form')]"))).click();
+    }
+}
