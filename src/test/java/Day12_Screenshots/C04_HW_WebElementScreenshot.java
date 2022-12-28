114ca2cacf8c1dc8fc43ceb89ff45e6164621c30
mehmet20251990 <mehmet20251990@gmail.com>
2022-12-28 22:29:09 +0300

    28.12 Update-3

--- src/test/java/Day12_Screenshots/C04_HW_WebElementScreenshot.java
+++ src/test/java/Day12_Screenshots/C04_HW_WebElementScreenshot.java
@@ -1,7 +1,15 @@
 package Day12_Screenshots;
 
 import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.io.FileHandler;
 import utilities.BaseTest;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class C04_HW_WebElementScreenshot extends BaseTest {
     /*
@@ -13,8 +21,15 @@ Save the screenshot with format 'screenshotname_yyyy-MM-dd_hh-mm-ss.png'
     public void tearDown() {
     }
     @Test
-    public void wEScshot() {
+    public void wEScshot() throws IOException {
         driver.get("https://opensource-demo.orangehrmlive.com/");
 
+        File source = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+
+        String date = new SimpleDateFormat("yyyy-MM-dd_hh-mm-ss").format(new Date());
+        String path = "test-output\\Screenshots\\oHRMPageScreenshot_" + date + ".png";
+
+        // Aldigimiz screenshot istedigimiz path e kaydediyoruz
+        FileHandler.copy(source, new File(path));
     }
 }
--- src/test/java/Day13_Log4j_SeleniumExceptions_ExtentReport/C01_Log4j.java
+++
@@ -1,4 +0,0 @@
-package Day13_Log4j_SeleniumExceptions_ExtentReport;
-
-public class C01_Log4j {
-}
